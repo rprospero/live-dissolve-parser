1907fe0bdf851933fdf5702cc9693cf23546048a
Adam Washington <adam.washington@stfc.ac.uk>
2022-02-15 16:54:48 +0000

    Add sites to species

--- src/Species.purs
+++ src/Species.purs
@@ -2,23 +2,37 @@ module Species where
 
 import Prelude
 import Control.Alternative ((<|>))
+import Data.Array (many)
+import Data.List.NonEmpty as List
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe)
 import Data.Show.Generic (genericShow)
 import Text.Parsing.Parser (Parser)
-import Text.Parsing.Parser.Combinators (optionMaybe)
-import Text.Parsing.Parser.String (char)
-import Util (dissolveTokens, signedFloat)
+import Text.Parsing.Parser.Combinators (many1Till, optionMaybe)
+import Text.Parsing.Parser.String (char, string)
+import Util (dissolveTokens, signedFloat, bool)
 
 data SpeciesPart
   = Atom Int String Number Number Number String (Maybe Number)
   | Angle Int Int Int String Number Number
   | Bond Int Int String Number Number
   | Isotopologue String String Int String Int
+  | Site String (Array SitePart)
 
 derive instance genericSpeciesPart :: Generic SpeciesPart _
 
 instance showSpeciesPart :: Show SpeciesPart where
+  show x = genericShow x
+
+data SitePart
+  = Origin (Array Int)
+  | XAxis (Array Int)
+  | YAxis (Array Int)
+  | OriginMassWeighted Boolean
+
+derive instance genericSitePart :: Generic SitePart _
+
+instance showSitePart :: Show SitePart where
   show x = genericShow x
 
 atom :: Parser String SpeciesPart
@@ -42,5 +56,23 @@ isotopologue = do
   nBeta <- dissolveTokens.integer
   pure $ Isotopologue name alpha nAlpha beta nBeta
 
+site :: Parser String SpeciesPart
+site = do
+  _ <- dissolveTokens.reserved "Site"
+  name <- dissolveTokens.stringLiteral
+  contents <- many1Till sitePart $ string "End"
+  _ <- dissolveTokens.reserved "Site"
+  pure (Site name $ List.toUnfoldable contents)
+
+origin = dissolveTokens.symbol "Origin" *> (Origin <$> many dissolveTokens.integer)
+
+xaxis = dissolveTokens.symbol "XAxis" *> (XAxis <$> many dissolveTokens.integer)
+
+yaxis = dissolveTokens.symbol "YAxis" *> (YAxis <$> many dissolveTokens.integer)
+
+massWeighted = dissolveTokens.symbol "OriginMassWeighted" *> (OriginMassWeighted <$> bool)
+
+sitePart = xaxis <|> yaxis <|> massWeighted <|> origin
+
 speciesPart :: Parser String SpeciesPart
-speciesPart = atom <|> bond <|> angle <|> isotopologue
+speciesPart = atom <|> bond <|> angle <|> isotopologue <|> site
