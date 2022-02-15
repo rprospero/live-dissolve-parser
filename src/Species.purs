e6918bd6009437252ef74d54046d6f752c3bba52
Adam Washington <adam.washington@stfc.ac.uk>
2022-02-15 16:38:26 +0000

    Add isotopologues

--- src/Species.purs
+++ src/Species.purs
@@ -7,26 +7,40 @@ import Data.Maybe (Maybe)
 import Data.Show.Generic (genericShow)
 import Text.Parsing.Parser (Parser)
 import Text.Parsing.Parser.Combinators (optionMaybe)
+import Text.Parsing.Parser.String (char)
 import Util (dissolveTokens, signedFloat)
 
 data SpeciesPart
   = Atom Int String Number Number Number String (Maybe Number)
   | Angle Int Int Int String Number Number
   | Bond Int Int String Number Number
+  | Isotopologue String String Int String Int
 
 derive instance genericSpeciesPart :: Generic SpeciesPart _
 
 instance showSpeciesPart :: Show SpeciesPart where
   show x = genericShow x
 
-speciesPartAtom :: Parser String SpeciesPart
-speciesPartAtom = dissolveTokens.symbol "Atom" *> (Atom <$> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat <*> signedFloat <*> dissolveTokens.stringLiteral <*> optionMaybe signedFloat)
+atom :: Parser String SpeciesPart
+atom = dissolveTokens.symbol "Atom" *> (Atom <$> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat <*> signedFloat <*> dissolveTokens.stringLiteral <*> optionMaybe signedFloat)
 
-speciesPartBond :: Parser String SpeciesPart
-speciesPartBond = dissolveTokens.symbol "Bond" *> (Bond <$> dissolveTokens.integer <*> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat)
+bond :: Parser String SpeciesPart
+bond = dissolveTokens.symbol "Bond" *> (Bond <$> dissolveTokens.integer <*> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat)
 
-speciesPartAngle :: Parser String SpeciesPart
-speciesPartAngle = dissolveTokens.symbol "Angle" *> (Angle <$> dissolveTokens.integer <*> dissolveTokens.integer <*> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat)
+angle :: Parser String SpeciesPart
+angle = dissolveTokens.symbol "Angle" *> (Angle <$> dissolveTokens.integer <*> dissolveTokens.integer <*> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat)
+
+isotopologue :: Parser String SpeciesPart
+isotopologue = do
+  _ <- dissolveTokens.symbol "Isotopologue"
+  name <- dissolveTokens.stringLiteral
+  alpha <- dissolveTokens.identifier
+  _ <- char '='
+  nAlpha <- dissolveTokens.integer
+  beta <- dissolveTokens.identifier
+  _ <- char '='
+  nBeta <- dissolveTokens.integer
+  pure $ Isotopologue name alpha nAlpha beta nBeta
 
 speciesPart :: Parser String SpeciesPart
-speciesPart = speciesPartAtom <|> speciesPartBond <|> speciesPartAngle
+speciesPart = atom <|> bond <|> angle <|> isotopologue
