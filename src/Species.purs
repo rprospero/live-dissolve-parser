fcefb84637c39c9fcc0e0e5f8a367d1a9fdb124e
Adam Washington <adam.washington@stfc.ac.uk>
2022-02-15 16:13:51 +0000

    Final atom number is optional

--- src/Species.purs
+++ src/Species.purs
@@ -1,14 +1,16 @@
 module Species where
 
 import Prelude
-import Util (dissolveTokens, signedFloat)
 import Control.Alternative ((<|>))
 import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe)
 import Data.Show.Generic (genericShow)
 import Text.Parsing.Parser (Parser)
+import Text.Parsing.Parser.Combinators (optionMaybe)
+import Util (dissolveTokens, signedFloat)
 
 data SpeciesPart
-  = Atom Int String Number Number Number String Number
+  = Atom Int String Number Number Number String (Maybe Number)
   | Angle Int Int Int String Number Number
   | Bond Int Int String Number Number
 
@@ -18,7 +20,7 @@ instance showSpeciesPart :: Show SpeciesPart where
   show x = genericShow x
 
 speciesPartAtom :: Parser String SpeciesPart
-speciesPartAtom = dissolveTokens.symbol "Atom" *> (Atom <$> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat <*> signedFloat <*> dissolveTokens.stringLiteral <*> signedFloat)
+speciesPartAtom = dissolveTokens.symbol "Atom" *> (Atom <$> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat <*> signedFloat <*> dissolveTokens.stringLiteral <*> optionMaybe signedFloat)
 
 speciesPartBond :: Parser String SpeciesPart
 speciesPartBond = dissolveTokens.symbol "Bond" *> (Bond <$> dissolveTokens.integer <*> dissolveTokens.integer <*> dissolveTokens.identifier <*> signedFloat <*> signedFloat)
