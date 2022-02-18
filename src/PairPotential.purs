0aeca4a4bafbfd10a570cbc4227fa310df474ad3
Adam Washington <adam.washington@stfc.ac.uk>
2022-02-18 14:35:23 +0000

    Display pair potentials

--- src/PairPotential.purs
+++ src/PairPotential.purs
@@ -2,8 +2,13 @@ module PairPotential where
 
 import Prelude
 import Control.Alternative ((<|>))
+import Data.Argonaut.Core
+import Data.Argonaut.Encode
+import Data.Array (catMaybes, foldl, head, tail)
 import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe(..), maybe)
 import Data.Show.Generic (genericShow)
+import Foreign.Object
 import Util (bool, dissolveTokens, punt, signedFloat)
 
 data PairPart
@@ -38,3 +43,32 @@ coulombTruncation = dissolveTokens.symbol "CoulombTruncation" *> (CoulombTruncat
 parameters = punt "Parameters" Parameters
 
 pairPart = range <|> delta <|> short <|> coulomb <|> parameters <|> manualChargeSource <|> forceChargeSource <|> coulombTruncation
+
+----------------------------------------------------------------------------
+popOnPair :: Array PairPart -> Json -> Json
+popOnPair xs s = foldl go s xs
+  where
+  go s (Range x) = "range" := x ~> s
+
+  go s (Delta x) = "delta" := x ~> s
+
+  go s (IncludeCoulomb x) = "includeCoulomb" := x ~> s
+
+  go s (CoulombTruncation x) = "coulombTruncation" := x ~> s
+
+  go s (ShortRangeTruncation x) = "shortRangeTruncation" := x ~> s
+
+  go s (ManualChargeSource x) = "manualChargeSource" := x ~> s
+
+  go s (ForceChargeSource x) = "forceChargeSource" := x ~> s
+
+  go s (Parameters xs) = caseJsonObject s (insertParam xs) s
+
+  insertParam :: Array String -> Object Json -> Json
+  insertParam xs ob = case head xs of
+    Nothing -> fromObject ob
+    Just name ->
+      let
+        ps = maybe jsonEmptyObject identity $ lookup "parameters" ob
+      in
+        "parameters" := (name := tail xs ~> ps) ~> fromObject ob
--- src/Types.purs
+++ src/Types.purs
@@ -2,18 +2,19 @@ module Types where
 
 import Data.Argonaut.Core
 import Data.Argonaut.Encode
+import Foreign.Object
 import Master
+import PairPotential
 import Prelude
+import Species
 import Configuration (ConfigurationPart)
 import Control.Monad.State (execState, modify)
-import Data.Array (catMaybes, head)
+import Data.Array (catMaybes, foldl, head, tail)
 import Data.Generic.Rep (class Generic)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Data.Show.Generic (genericShow)
 import Data.Tuple (Tuple(..))
 import Layer (LayerPart)
-import PairPotential (PairPart)
-import Species (SpeciesPart)
 
 data Section
   = Species String (Array SpeciesPart)
@@ -36,10 +37,29 @@ instance encodeDissolve :: EncodeJson Dissolve where
       $ do
           _ <- modify (\c -> "config" := "Config" ~> c)
           _ <- modify (\c -> "layer" := "Layer" ~> c)
-          _ <- modify (\c -> "pair" := "pair" ~> c)
+          _ <- modify (writePair pair)
           _ <- modify (\c -> "species" := "species" ~> c)
           _ <- modify (writeMaster master)
           pure 7
+
+writePair :: Maybe (Array PairPart) -> Json -> Json
+writePair Nothing s = s
+
+writePair (Just xs) s = "PairPotentials" := (popOnPair xs jsonEmptyObject) ~> s
+
+writeMaster (Just xs) s =
+  "Master"
+    := ( "impropers"
+          := (catMaybes $ map getImproper $ xs)
+          ~> "torsions"
+          := (catMaybes $ map getTorsion $ xs)
+          ~> "bond"
+          := (catMaybes $ map getBond $ xs)
+          ~> "angle"
+          := (catMaybes $ map getAngle $ xs)
+          ~> jsonEmptyObject
+      )
+    ~> s
 
 writeMaster Nothing s = s
 
