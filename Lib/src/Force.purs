370e0d90cf3450175a07226bf4ccf9820e030ec7
Adam Washington <adam.washington@stfc.ac.uk>
2022-02-28 10:32:38 +0000

    Add Cos2 and UFFCosine

--- Lib/src/Force.purs
+++ Lib/src/Force.purs
@@ -22,10 +22,12 @@ import Util (arbitrary, dissolveTokens, named, signedFloat, signedNum)
 data ForceInfo
   = Harmonic Number Number
   | Ref String
+  | Cos2 Number Number Number Number
   | Cos3 Number Number Number
   | Cos Number Number Number Number
   | CosN (Array Number)
   | CosNC (Array Number)
+  | UFFCosine Number Number Number
   | LJ Number Number
   | LJGeometric (Array Number)
   | None
@@ -35,11 +37,13 @@ derive instance genericForceInfo :: Generic ForceInfo _
 instance showForceInfo :: Show ForceInfo where
   show x = genericShow x
 
-forceInfo = harmonic <|> ref <|> none <|> cos3 <|> cosNC <|> cosN <|> cos <|> ljGeometric <|> lj <?> "Force Info"
+forceInfo = harmonic <|> ref <|> none <|> cos2 <|> cos3 <|> cosNC <|> cosN <|> cos <|> uffCosine <|> ljGeometric <|> lj <?> "Force Info"
   where
   harmonic = dissolveTokens.symbol "Harmonic" *> (Harmonic <$> named signedNum <*> named signedNum)
 
+  cos2 = dissolveTokens.symbol "Cos2" *> (Cos2 <$> named signedNum <*> named signedNum <*> named signedNum <*> named signedNum)
   cos3 = dissolveTokens.symbol "Cos3" *> (Cos3 <$> named signedNum <*> named signedNum <*> named signedNum)
+  uffCosine = dissolveTokens.symbol "UFFCosine" *> (UFFCosine <$> named signedNum <*> named signedNum <*> named signedNum)
 
   cosNC = dissolveTokens.symbol "CosNC" *> ((CosNC <<< toUnfoldable) <$> (sepBy1 signedFloat dissolveTokens.whiteSpace))
 
@@ -119,6 +123,28 @@ writeRef (Just (Cos3 i j k)) s =
     := i
     ~> s
 
+writeRef (Just (UFFCosine i j k)) s =
+  "type" := "UFFCosine"
+    ~> "k"
+    := k
+    ~> "j"
+    := j
+    ~> "i"
+    := i
+    ~> s
+
+writeRef (Just (Cos2 i j k l)) s =
+  "type" := "Cos2"
+    ~> "l"
+    := l
+    ~> "k"
+    := k
+    ~> "j"
+    := j
+    ~> "i"
+    := i
+    ~> s
+
 writeRef (Just (Ref name)) s =
   "type" := "Reference"
     ~> "master"
@@ -135,8 +161,11 @@ xmlRef (Just (Ref name)) = onNewChild "reference" $ onAttr "master" name
 xmlRef (Just (Harmonic k eq)) = onNewChild "harmonic" $ onAttr "eq" eq *> onAttr "k" k
 
 xmlRef (Just (Cos i j k l)) = onNewChild "cos" $ onAttr "i" i *> onAttr "j" j *> onAttr "k" k *> onAttr "l" l
+xmlRef (Just (Cos2 i j k l)) = onNewChild "cos3" $ onAttr "i" i *> onAttr "j" j *> onAttr "k" k *> onAttr "l" l
 
 xmlRef (Just (Cos3 i j k)) = onNewChild "cos3" $ onAttr "i" i *> onAttr "j" j *> onAttr "k" k
+
+xmlRef (Just (UFFCosine i j k)) = onNewChild "uffCosine" $ onAttr "i" i *> onAttr "j" j *> onAttr "k" k
 
 xmlRef (Just (LJ a b)) = onNewChild "lj" $ onAttr "a" a *> onAttr "b" b
 
